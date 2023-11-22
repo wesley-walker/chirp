c21b0e5f4d548cc3d9aa25c11ab6b242fba3f987
Wesley Walker <wesleywalker.code@gmail.com>
2023-11-21 20:41:57 -0500

    map post db content to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
+  const user = useUser();
+  const {data} = api.post.getLatest.useQuery();
   return (
     <>
       <Head>
@@ -15,7 +15,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <SignIn />;
+      <div>
+          {!user.isSignedIn && <SignIn/>}
+          {!!user.isSignedIn && <SignOutButton/>}
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
+      </div>
       </main>
     </>
   );
