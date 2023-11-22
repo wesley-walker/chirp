92f5fdeb8c09c5eadbfc7697ebbfa09574fad0fd
Wesley Walker <wesleywalker.code@gmail.com>
2023-11-21 20:04:19 -0500

    quit()

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import { type AppType } from "next/app";
+import '~/styles/globals.css'
+import { api } from "~/utils/api"
+import '~/styles/globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
 
-import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
 export default api.withTRPC(MyApp);
